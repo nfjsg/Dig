14be76d48edcf4025d6f9fb1ef1e4bda9eded573
nfjsg <dasarathan@icloud.com>
2024-01-12 21:50:28 -0500

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,1 +1,19 @@
 
+// src/components/Navigation.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        {/* Add more navigation links based on your project structure */}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
