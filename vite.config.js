c9edc59b7eb08ce5d575e3e79978ea97095a837d
nfjsg <dasarathan@icloud.com>
2024-01-12 21:52:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+// vite.config.js
+import ReactRefresh from '@vitejs/plugin-react-refresh';
 
+export default {
+  plugins: [ReactRefresh()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+};
