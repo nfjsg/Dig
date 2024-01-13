453c2995464511f278a4384e291f33289c2b8696
nfjsg <dasarathan@icloud.com>
2024-01-12 21:50:16 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,1 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Our Website</h2>
+      <p>This is the home page content. Feel free to explore and discover more about us.</p>
+    </div>
+  );
+}
+
+export default Home;
