befe0f077b16ae134436459bbcd1cc3b8adead81
nfjsg <dasarathan@icloud.com>
2024-01-12 21:50:03 -0500

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,1 +1,13 @@
+// src/components/Error.jsx
+import React from 'react';
 
+const Error = () => {
+  return (
+    <div>
+      <h2>Oops! Something went wrong.</h2>
+      <p>We apologize for the inconvenience. Please try again later.</p>
+    </div>
+  );
+}
+
+export default Error;
