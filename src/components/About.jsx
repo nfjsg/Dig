1d03f99f4dddc938a69cd01d17553ce0b33f1a97
nfjsg <dasarathan@icloud.com>
2024-01-12 21:49:21 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,1 +1,12 @@
+// src/components/About.jsx
+import React from 'react';
 
+const About = () => {
+  return (
+    <div>
+      {/* About component content */}
+    </div>
+  );
+}
+
+export default About;
