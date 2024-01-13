f27a9473d9c477b223f03197044b3895af934bcf
nfjsg <dasarathan@icloud.com>
2024-01-12 21:50:41 -0500

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,1 +1,15 @@
 
+// src/components/Project.jsx
+import React from 'react';
+
+const Project = () => {
+  return (
+    <div>
+      <h2>Our Projects</h2>
+      <p>Explore our featured projects and discover our work in different areas.</p>
+      {/* Add project details and images as needed */}
+    </div>
+  );
+}
+
+export default Project;
